74556c97497d292d58af92b82d0df79db555b1eb
chdo002 <1107661983@qq.com>
2018-05-12 18:55:43 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,6 +1,15 @@
+
+
 param='update';
 if [[ $1 ]]; then
 	param=$1;
+fi
+
+if [[ $param == 'update' ]]; then
+	read -p '输入commit说明(默认update):' des;	
+	if [[ $des ]]; then
+		param=$des;
+	fi
 fi
 
 git add -A;
@@ -10,3 +19,5 @@ git push origin;
 hexo c;
 hexo g;
 hexo d;
+
+echo 'commit为：$param';
